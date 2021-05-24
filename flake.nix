de6d64973c4a61d92c6cc419401d39fd8a3c471e
Euan Kemp <euank@euank.com>
2021-05-23 20:56:25 -0700

    crd2pulumi: init at v1.0.7

--- flake.nix
+++ flake.nix
@@ -8,6 +8,8 @@
       pulumi-sdk = final.callPackage ./pkgs/tools/admin/pulumi/sdk.nix {};
 
       kube2pulumi = final.callPackage ./pkgs/tools/admin/kube2pulumi {};
+
+      crd2pulumi = final.callPackage ./pkgs/tools/admin/crd2pulumi {};
     };
 
     packages.x86_64-linux = (import nixpkgs { system = "x86_64-linux"; overlays = [ self.overlay ]; });
---
+++ pkgs/tools/admin/crd2pulumi/default.nix
@@ -0,0 +1,24 @@
+{ lib, fetchFromGitHub, buildGoModule }:
+
+buildGoModule rec {
+  pname = "crd2pulumi";
+  version = "1.0.7";
+
+  src = fetchFromGitHub {
+    owner = "pulumi";
+    repo = "crd2pulumi";
+    rev = "v${version}";
+    sha256 = "sha256-58p7/W+47nTjJHIq3KmeB4+jLmgvDtM7UA+NZHB5j8c=";
+  };
+  vendorSha256 = "sha256-baTGg8kydp9FxxsGiP7SCQGhgODs+4fH7dkS74TaJXY=";
+
+  doCheck = false;
+
+  meta = with lib; {
+    description = "Generate typed CustomResources from a Kubernetes CustomResourceDefinition";
+    homepage = "https://www.pulumi.com/";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ euank ];
+    platforms = platforms.unix;
+  };
+}
